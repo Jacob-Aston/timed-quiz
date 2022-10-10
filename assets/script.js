bcf16eda67997f739d648fce84e1a422ccbb111f
JAston <jacob.aston1@gmail.com>
2022-10-09 22:18:45 -0600

    Added hide class to reveal questions after start.

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
-var countdown = 10;
+var countdown = 13;
 
 //https://theprogrammingexpert.com/javascript-countdown-timer/
 const timero = function() {
 var timer = setInterval(function() {
     document.getElementById('time').innerHTML = "<b>" + countdown + "</b>";
     countdown--
-    if(countdown == 0){
-        document.getElementById('time').innerHTML = "";
+    if(countdown == -1){
         clearInterval(timer)
+        // document.getElementById('time').innerHTML = "";
     }
 
     
@@ -15,13 +15,13 @@ var timer = setInterval(function() {
 }
 
 
-const initButton = () => {
+const initButton = (hide) => {
     const element = document.getElementById('start-btn');
     element.addEventListener("click",  function() {
-        document.getElementsByClassName("question").classList.remove("hide");
-        document.getElementsByClassName("question-box").classList.remove("hide");
-        countdown = 12;
         timero();
+        document.getElementById("question").classList.remove("hide");
+        document.getElementById("question-box").classList.remove("hide");
+        document.getElementById("start-btn").classList.add("hide");
     });
 }
 
