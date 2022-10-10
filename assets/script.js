92e66dad0bad3b69d07cbc57562de1fc52b24350
JAston <jacob.aston1@gmail.com>
2022-10-09 19:44:16 -0600

    Added a countdown that shows after pressing start.

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,31 @@
+var countdown = 10;
+
+//https://theprogrammingexpert.com/javascript-countdown-timer/
+const timero = function() {
+var timer = setInterval(function() {
+    countdown--
+    document.getElementById('time').innerHTML = "<b>" + countdown + "</b>";
+    if(countdown == 0){
+        document.getElementById('time').innerHTML = "";
+        clearInterval(timer)
+    }
+
+    
+}, 1000);
+}
+
 const initButton = () => {
     const element = document.getElementById('start-btn');
-
-    console.log('hi');
-
     element.addEventListener("click",  function() {
-        console.log('howdy');
+        countdown = 12;
+        timero();
+
     });
 }
 
+
 const init = () => {
  initButton();
-
 }
 
 window.onload = init;
