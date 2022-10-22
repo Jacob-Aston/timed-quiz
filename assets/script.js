bd306f12b3312dbdf6396c30c09ea75f12796ce4
JAston <jacob.aston1@gmail.com>
2022-10-21 21:47:05 -0600

    quiz now deducts time when incorrect answer is clicked.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
-var countdown = 13;
+var countdown = 30;
 const quiz = document.getElementById("quiz");
+var questionBoxEl;
+
 //https://theprogrammingexpert.com/javascript-countdown-timer/
 const timero = function () {
   var timer = setInterval(function () {
@@ -16,28 +18,28 @@ const questions = [
   {
     prompt: "question 1",
     answers: [
-      { text: "1", correct: true },
-      { text: "2", correct: false },
-      { text: "3", correct: false },
-      { text: "4", correct: false },
+      { id:"0", text: "1", correct: false },
+      { id:"1", text: "2", correct: true },
+      { id:"2", text: "3", correct: false },
+      { id:"3", text: "4", correct: false },
     ],
   },
   {
     prompt: "question 2",
     answers: [
-      { text: "1", correct: true },
-      { text: "2", correct: false },
-      { text: "3", correct: false },
-      { text: "4", correct: false },
+      { id:"0", text: "1", correct: true },
+      { id:"1", text: "2", correct: false },
+      { id:"2", text: "3", correct: false },
+      { id:"3", text: "4", correct: false },
     ],
   },
   {
     prompt: "question 3",
     answers: [
-      { text: "1", correct: true },
-      { text: "2", correct: false },
-      { text: "3", correct: false },
-      { text: "4", correct: false },
+      { id:"0", text: "1", correct: true },
+      { id:"1", text: "2", correct: false },
+      { id:"2", text: "3", correct: false },
+      { id:"3", text: "4", correct: false },
     ],
   },
 ];
@@ -55,6 +57,7 @@ const init = () => {
   addStartButton();
 };
 
+//generates and displays answer elements on question box
 const addAnswers = () => {
   let ans = questions[0].answers;
   let questionBox = document.getElementById("question-box");
@@ -63,10 +66,36 @@ const addAnswers = () => {
     let ansEl = document.createElement("div");
     ansEl.setAttribute("class", "answer");
     ansEl.innerHTML = ans[i].text;
+    if( ans[i].correct == true) {
+      ansEl.dataset.correct = true;
+    };
+    if( ans[i].correct == false) {
+      ansEl.dataset.correct = false;
+    };
     questionBox.append(ansEl);
+
+    ansEl.addEventListener('click', function(e){
+      const answer = e.target;
+      const answerCorrect = answer.dataset.correct;
+      console.log(answer);
+      console.log(answerCorrect);
+      if( answerCorrect == 'true' ) {
+        //display correct
+      }
+      if( answerCorrect == 'false' ) {
+        countdown -= 5;
+        //display incorrect
+      }
+      //nextQuestion();
+    });
+    //add event listener here & console.log
+    //grab content from individual div
+    // progress index
+    //correct check...
   }
 };
 
+//generates and adds a question box div with a question element child
 const addQuestions = () => {
   let questionBox = document.createElement("div");
   let questionEl = document.createElement("h2");
@@ -76,24 +105,26 @@ const addQuestions = () => {
   quiz.append(questionBox);
 };
 
+//adds a timer element to quiz
 const addTimer = () => {
   let timeEl = document.createElement("div");
   timeEl.setAttribute("id", "time");
   quiz.append(timeEl);
 };
 
-const clickAnswer = (event) => {
-  let target = event.target;
-  console.log(target);
-};
-
+//adds timer el question box with question and answers. Also starts timer
 const startQuiz = () => {
   addTimer();
   addQuestions();
   addAnswers();
   timero();
+  // questionBoxEl = document.getElementById('question-box');
 };
 
+// questionBoxEl.addEventListener('click', function() {
+// console.log('click')
+// });
+//Starts quiz when start button is pressed
 quiz.addEventListener("click", function (event) {
   let target = event.target;
   let targetClass = event.target.classList.value;
